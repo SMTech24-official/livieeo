9b36cc4c3effaaf031a764a20550c8d24fc305c3
Azhar Mahmud <azharmahmud730@gmail.com>
2025-09-07 03:58:31 +0600

    some changes

--- src/shared/prisma.ts
+++ src/shared/prisma.ts
@@ -8,7 +8,5 @@ declare global {
 const prisma = global.prisma || new PrismaClient();
  
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
-// hi helo
+
 export default prisma;
- 
- 
